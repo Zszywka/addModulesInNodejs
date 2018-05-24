bdb0edd773577c7cc02afeb3249104ea2af7b757
Joanna Piotrowska <PiotrowskaWIMIIP@gmail.com>
2018-05-24 22:17:54 +0200

    Init project

---
+++ app/program.js
@@ -0,0 +1,26 @@
+// 'os' module import
+var os = require('os');
+
+process.stdin.setEncoding('utf-8');
+process.stdin.on('readable', function() {
+  var input = process.stdin.read();
+  if(input !== null) {
+    var instruction = input.trim();
+    switch(instruction) {
+      case 'exit':
+        process.stdout.write('Quitting app!\n');
+        process.exit();
+        break;
+      case 'sayhello':
+        process.stdout.write('hello!\n');
+        break;
+      case 'getOSinfo()'
+        break;
+      default:
+        process.stderr.write('Wrong instruction!\n');
+    };
+  }
+});
+
+// var OSinfo = require('./OSinfo');
+var OSinfo = require('../modules/OSInfo');
